70e3a3312129555ad78a9b90b4a45b1d2f5d7c6d
D <MacMini@Abhijits-Mac-mini.local>
2016-11-10 00:02:14 -0800

    fixes

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,5 +1,38 @@
 import media
+import fresh_tomatoes
 
 
-avatar = media.Movie("Avatar")
-spotlight = media.Movie("Spotlight")
+avatar = media.Movie(
+    "Avatar",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+    )
+
+spotlight = media.Movie(
+    "Spotlight",
+    "https://www.youtube.com/watch?v=rfm_wJFerfA"
+    )
+
+deadpool = media.Movie(
+    "Deadpool",
+    "https://www.youtube.com/watch?v=ONHBaC-pfsk"
+    )
+
+strange = media.Movie(
+    "Doctor Strange",
+    "https://www.youtube.com/watch?v=HSzx-zryEgM"
+    )
+
+dory = media.Movie(
+    "Finding Dory",
+    "https://www.youtube.com/watch?v=TAXZRz_j5AQ"
+    )
+
+accountant = media.Movie(
+    "The Accountant",
+    "https://www.youtube.com/watch?v=DBfsgcswlYQ"
+    )
+
+
+movies = [avatar, spotlight, deadpool, strange, dory, accountant]
+
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -4,19 +4,22 @@ import os
 
 
 class Movie:
-    def __init__(self, title):
+    def __init__(self, title, trailer_youtube_url):
         self.title = title
+        self.trailer_youtube_url = trailer_youtube_url
         Movie.download_data(self)
 
     def download_data(self):
         url = "http://www.omdbapi.com/?t="+self.title
+        url = url.replace(" ", "%20")
         response = urlopen(url)
         data = json.loads(response.read().decode("utf-8"))
         self.storyline = data['Plot']
-        self.box_art = data['Poster']       
-        image_path = self.title+".jpg"        
+        self.box_art = data['Poster']
+        image_path = self.title+".jpg"
         if not(os.path.isfile(image_path)):
             box_art_response = urlopen(self.box_art)
             image_file = open(image_path, 'w')
             image_file.write(box_art_response.read())
             image_file.close()
+        self.poster_image_url = image_path
